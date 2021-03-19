2007478d6884ef34dce8ef38456567ec663fc5db
Guilherme Santos <guilherme.santos@doclerholding.com>
2021-03-19 15:47:52 +0100

    Observable implementation

--- src/lib/observable.ts
+++ src/lib/observable.ts
@@ -1,1 +1,11 @@
-export const Observable: any = null;
+import { IObservable, IObserver, IProducer, ISubscription } from "../types";
+
+export const Observable = (producer: IProducer): IObservable => {
+    const subscribe = (observer: IObserver): ISubscription => {
+        return producer(observer);
+    };
+
+    return {
+        subscribe
+    }
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export interface IObserver<T = any> {
   next(value: T): any;
-  error(error: any): any;
-  complete(): any;
+  error?(error: any): any;
+  complete?(): any;
 }
 
 export interface ISubscription {
@@ -10,4 +10,8 @@ export interface ISubscription {
 
 export interface IProducer {
   (observer: IObserver): ISubscription;
+}
+
+export interface IObservable {
+  subscribe(observer: IObserver): ISubscription;
 }
