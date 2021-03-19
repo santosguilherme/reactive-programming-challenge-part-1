ccc2b283fc343f39bfdc2b810f0063e1695cb299
Guilherme Santos <guilherme.santos@doclerholding.com>
2021-03-19 16:38:16 +0100

    fromEvent implementation

--- src/lib/fromEvent.ts
+++ src/lib/fromEvent.ts
@@ -1,6 +1,25 @@
+import { IObservable, IObserver, IProducer } from "../types";
+import { Observable } from "./observable";
+
 export function fromEvent(
   nodeEl: HTMLElement,
   eventName: "error" | "click" | "change" | "input"
-) {
-  return null;
+): IObservable {
+  const producer: IProducer = (observer: IObserver) => {
+    const eventHandler = (event: MouseEvent) => observer.next(event);
+
+    if (typeof nodeEl.addEventListener === 'function') {
+      nodeEl.addEventListener(eventName, eventHandler);
+    } else {
+      observer.error('You should provide a valid html element');
+    }
+
+    observer.complete();
+
+    return {
+      unsubscribe: () => nodeEl.removeEventListener(eventName, eventHandler)
+    };
+  };
+
+  return new Observable(producer);
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,19 +1,2 @@
-import { IObservable, IObserver, IProducer } from "../types";
-import { Observable } from "./observable";
-
 export * from "./observable";
-
-// TODO: find a type from evenType param
-export const fromEvent = (element: HTMLElement, eventType: string): IObservable => {
-    const producer: IProducer = (observer: IObserver) => {
-        const eventHandler = (event: MouseEvent) => observer.next(event);
-
-        element.addEventListener(eventType, eventHandler);
-
-        return {
-            unsubscribe: () => element.removeEventListener(eventType, eventHandler)
-        };
-    };
-
-    return new Observable(producer);
-};
+export * from "./fromEvent";
